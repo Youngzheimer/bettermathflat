bee2d3aaf5fe6382725d492ea490990719de9720
youngzheimer <youngzheimer@gmail.com>
2025-02-24 02:07:12 +0900

    Initial Commit

---
+++ bettermathflat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bettermathflat
+//
+//  Created by youngzheimer on 2/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bettermathflat/bettermathflatApp.swift
@@ -0,0 +1,17 @@
+//
+//  bettermathflatApp.swift
+//  bettermathflat
+//
+//  Created by youngzheimer on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct bettermathflatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
